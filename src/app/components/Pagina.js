5a91db2482382db2f1e37778059161245b708f80
Rafael Goncalves Nobre <rafael.nobre@iesb.br>
2024-11-19 18:51:22 -0300

    .

---
+++ src/app/components/Pagina.js
@@ -0,0 +1,46 @@
+import {
+  Container,
+  Nav,
+  NavDropdown,
+  NavLink,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarToggle,
+} from "react-bootstrap";
+import Image from "next/image";
+import logo from "../components/logo.png";
+import styles from "./Pagina.module.css";
+
+export default function Pagina(props) {
+  return (
+    <>
+      <Navbar className={styles.navbar} expand="lg">
+        <Container fluid>
+          <NavbarBrand className={styles.brand} href="/">
+            <Image src={logo} alt="Logo" height={80} />
+          </NavbarBrand>
+          <Nav className="me-end">
+            <NavLink className={styles.navLink} href="/vinhos">
+              Vinhos
+            </NavLink>
+            <NavLink className={styles.navLink} href="/clientes">
+              Clientes{" "}
+            </NavLink>
+            <NavLink className={styles.navLink} href="/fornecedores">
+              Fornecedores{" "}
+            </NavLink>
+            <NavLink className={styles.navLink} href="/pedidos">
+              Pedidos
+            </NavLink>
+            <NavLink className={styles.navLink} href="/estoque">
+              Estoque
+            </NavLink>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Container>{props.children}</Container>
+    </>
+  );
+}
